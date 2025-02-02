edce69877696bf641c32abe64d45c3837bc42c64
MateuszKruk <makoo93@gmail.com>
2025-02-02 21:59:13 +0100

    Simplify method

--- src/main/java/pl/mateuszkruk/UniversalMethods/RemoveEmployeeWorkedManyDaysInARow.java
+++ src/main/java/pl/mateuszkruk/UniversalMethods/RemoveEmployeeWorkedManyDaysInARow.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 import pl.mateuszkruk.Employee.Employee;
 import pl.mateuszkruk.Schedule.Shifts;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,36 +14,23 @@ public class RemoveEmployeeWorkedManyDaysInARow {
     public static void remove(int dayOfMonth,
                               Map<Integer, Map<Employee, Shifts>> finalSchedule,
                               List<Employee> copyEmployeeList) {
-        int DaysInARow = 5;
+
+
         Preconditions.checkArgument(dayOfMonth > 0, "Day cannot be zero or less!");
         Preconditions.checkNotNull(finalSchedule, "Monthly Schedule cannot be null!");
         Preconditions.checkNotNull(copyEmployeeList, "List of employees cannot be null!");
 
+        List<Employee> workingList = new ArrayList<>(copyEmployeeList);
 
-        if (dayOfMonth > DaysInARow) {
-            Map<Employee, Integer> whenEmployeeWorked = new HashMap<>();
+        for (Employee employee : workingList) {
+            if (    finalSchedule.get(dayOfMonth-1).containsKey(employee) &&
+                    finalSchedule.get(dayOfMonth-2).containsKey(employee) &&
+                    finalSchedule.get(dayOfMonth-3).containsKey(employee) &&
+                    finalSchedule.get(dayOfMonth-4).containsKey(employee)){
 
-            for (Map.Entry<Integer, Map<Employee, Shifts>> entry : finalSchedule.entrySet()) {
-                Integer day = entry.getKey();
-                Map<Employee, Shifts> employeeShift = entry.getValue();
-
-                if (day > (dayOfMonth - (DaysInARow-1))) {
-                    for (Map.Entry<Employee, Shifts> workingMap : employeeShift.entrySet()) {
-                        Employee singleEmployee = workingMap.getKey();
-
-                        whenEmployeeWorked.put(singleEmployee, whenEmployeeWorked.getOrDefault(singleEmployee, 0) + 1);
-                    }
-                }
-            }
-
-            for (Map.Entry<Employee, Integer> numberOfDaysEmployeeWorked : whenEmployeeWorked.entrySet()) {
-                Employee employeeToRemove = numberOfDaysEmployeeWorked.getKey();
-
-                if (numberOfDaysEmployeeWorked.getValue() == DaysInARow) {
-                    copyEmployeeList.remove(employeeToRemove);
-                }
-
+                copyEmployeeList.remove(employee);
             }
         }
     }
+
 }
