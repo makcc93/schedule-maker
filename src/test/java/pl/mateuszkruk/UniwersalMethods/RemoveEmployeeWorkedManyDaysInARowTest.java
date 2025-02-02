3fc6413c345c371ae80542b7101a37e673926c63
MateuszKruk <makoo93@gmail.com>
2025-02-02 22:00:00 +0100

    Add tests

---
+++ src/test/java/pl/mateuszkruk/UniwersalMethods/RemoveEmployeeWorkedManyDaysInARowTest.java
@@ -0,0 +1,54 @@
+package pl.mateuszkruk.UniwersalMethods;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.mateuszkruk.Employee.Employee;
+import pl.mateuszkruk.Schedule.Shifts;
+import pl.mateuszkruk.UniversalMethods.RemoveEmployeeWorkedManyDaysInARow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RemoveEmployeeWorkedManyDaysInARowTest {
+    Map<Integer, Map<Employee, Shifts>> schedule = new HashMap<>();
+    Employee employee1 = new Employee("1", "Adam", "Nowak", false, true);
+    Employee employee2 = new Employee("2", "Tomasz", "Wiśniewski", false, false);
+    Employee employee3 = new Employee("3", "Dawid", "Kowalski", true, false);
+
+    List<Employee> employees = new ArrayList<>();
+
+    @BeforeEach
+    public void setup(){
+
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+    }
+
+    @Test
+    public void removingWorkedLast5Days(){
+        int day = 6;
+        Map<Employee,Shifts> insideMap = new HashMap<>();
+        Shifts allDay = Shifts.EIGHT_TO_TWENTY;
+        insideMap.put(employee2,allDay);
+
+        schedule.put(1,insideMap);
+        schedule.put(2,insideMap);
+        schedule.put(3,insideMap);
+        schedule.put(4,insideMap);
+        schedule.put(5,insideMap);
+
+        RemoveEmployeeWorkedManyDaysInARow.remove(day,schedule,employees);
+
+        List<Employee> expectedList = List.of(employee1,employee3);
+
+        assertEquals(expectedList,employees);
+
+    }
+}
