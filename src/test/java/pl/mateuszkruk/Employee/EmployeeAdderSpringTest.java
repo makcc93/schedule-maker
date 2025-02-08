8a9dde6f8ad666857339233febb6c6e9f9a3cbd7
MateuszKruk <makoo93@gmail.com>
2025-02-08 23:34:05 +0100

    first test in spring

--- src/test/java/pl/mateuszkruk/Employee/EmployeeAdderSpringTest.java
+++ src/test/java/pl/mateuszkruk/Employee/EmployeeAdderSpringTest.java
@@ -1,6 +1,5 @@
 package pl.mateuszkruk.Employee;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.mateuszkruk.UserInput.InputHandler;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,26 +47,36 @@ public class EmployeeAdderSpringTest {
     }
 
     @Test
-    public void executeTestWithNullEmployee() throws Exception{
-        when(inputHandler.getString("Podaj numer SAP pracownika, którego chcesz dodać:")).thenReturn(null);
+    public void executeTestWithEmptyEmployeeValues() throws Exception{
+        when(inputHandler.getString("Podaj numer SAP pracownika, którego chcesz dodać:"))
+                .thenReturn("")
+                .thenReturn("123");
 
-        when(employeeListsMatcher.getAllEmployees().contains(employeeListsMatcher.getEmployee(null))).thenReturn(null);
+        when(employeeListsMatcher.getAllEmployees().contains(employeeListsMatcher.getEmployee("")))
+                .thenReturn(null);
 
-        when(inputHandler.getString("Podaj imię pracownika:")).thenReturn(null);
+        when(inputHandler.getString("Podaj imię pracownika:"))
+                .thenReturn("")
+                .thenReturn("Adam");
 
-        when(inputHandler.getString("Podaj nazwisko pracownika:")).thenReturn(null);
+        when(inputHandler.getString("Podaj nazwisko pracownika:"))
+                .thenReturn("")
+                .thenReturn("Smith");
 
-        when(inputHandler.getBoolean("Jeśli pracownik może sporządzać wnioski ratalne wciśnij 1, jeśli nie wciśnij 0")).thenReturn(true);
+        when(inputHandler.getBoolean("Jeśli pracownik może sporządzać wnioski ratalne wciśnij 1, jeśli nie wciśnij 0"))
+                .thenReturn(true);
 
-        when(inputHandler.getBoolean("Jeśli pracownik jest kierownikiem wciśnij 1, jeśli nie wciśnij 0")).thenReturn(false);
+        when(inputHandler.getBoolean("Jeśli pracownik jest kierownikiem wciśnij 1, jeśli nie wciśnij 0"))
+                .thenReturn(false);
 
-        when(inputHandler.getBoolean("Jeśli potwiedzasz wciśnij 1, jeśli nie wciśnij 0")).thenReturn(true);
+        when(inputHandler.getBoolean("Jeśli potwiedzasz wciśnij 1, jeśli nie wciśnij 0"))
+                .thenReturn(true);
 
-        when(inputHandler.getBoolean("Jeśli chcesz dodawać kolejnego pracownika wciśnij 1, jeśli chcesz wyjść wciśnij 0.")).thenReturn(false);
+        when(inputHandler.getBoolean("Jeśli chcesz dodawać kolejnego pracownika wciśnij 1, jeśli chcesz wyjść wciśnij 0."))
+                .thenReturn(false);
 
         employeeAdder.execute(employeeListsMatcher,inputHandler);
 
-        verify(employeeListsMatcher,times(1)).removeEmployeeFromList(any(Employee.class));
+        verify(employeeListsMatcher,never()).removeEmployeeFromList(any(Employee.class));
     }
-
 }
